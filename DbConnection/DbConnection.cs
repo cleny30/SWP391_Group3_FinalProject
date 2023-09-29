2d89f157fa140b91a6e66a73630c1b1072d46e6e
Cleny Nguyen <kushirotm@gmail.com>
2023-09-29 14:59:42 +0700

    upload dbConnection

---
+++ DbConnection/DbConnection.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace SWP391_Group3_FinalProject.NewFolder
+{
+    public class DbConnection
+    {
+        private static string connectionString = "Data Source=CLENY\\CLENYSQL;Initial Catalog=SWPDatabase;Persist Security Info=True;User ID=sa;Password=12";
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
