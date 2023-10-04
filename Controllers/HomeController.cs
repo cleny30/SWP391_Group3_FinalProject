907ecda7855c67332928f60148506fb5b7dcec9e
Cleny Nguyen <kushirotm@gmail.com>
2023-10-04 15:53:43 +0700

    Upload, Fix Mainpage, Shop list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,17 +13,33 @@ namespace SWP391_Group3_FinalProject.Controllers
         {
             ProductDAO dao = new ProductDAO();
             List<Product> list = dao.GetAllProduct();
-            List<Product> listMouse = dao.getMouse();
-            List<Product> listKeyboard = dao.getAllKeyboard();
+            List<Product> listMouse = list.Where(pro => pro.cate_id == 2).ToList();
+            List<Product> listKeyboard = list.Where(pro => pro.cate_id == 1).ToList();
+            List<Brand> brandList = dao.GetAllBrand();
+            List<Category> cateList = dao.GetAllCategory();
+
+            List<int> totalProductBrand = new List<int>();
+            foreach (Brand brand in brandList)
+            {
+                totalProductBrand.Add(list.Count(pro => pro.brand_id == brand.brand_id));
+            }
+
+            List<int> totalProductCate = new List<int>();
+            foreach (Category cate in cateList)
+            {
+                totalProductCate.Add(list.Count(pro => pro.cate_id == cate.cate_id));
+            }
+
+            ViewBag.totalProductBrand = totalProductBrand;
+            ViewBag.totalProductCate = totalProductCate;
+            ViewBag.brandList = brandList;
+            ViewBag.cateList = cateList;
             ViewBag.listMouse = listMouse;
             ViewBag.listKeyboard = listKeyboard;
             ViewBag.list = list;
+
             return View();
+
         }
     }
-
-
-
-
-
 }
