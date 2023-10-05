1e15118c80f5d19c49242cadcf9d252974a68cc7
Nguyen Huu Duy <tin66hv@gmail.com>
2023-10-05 17:05:17 +0700

    Finish Import Product, add Models for Import Reciept and Receipt Product and add ImportRecieptDAO and add class to ProductDAO

---
+++ Models/Receipt_Product.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlTypes;
+
+namespace SWP391_Group3_FinalProject.Models
+{
+    public class Receipt_Product
+    {
+        //public int Reciept_ID { get; set; }
+        public string pro_id { get; set; }
+
+        public string pro_name { get; set;}
+
+        public int amount { get; set;}
+
+        public int price { get; set;}  
+    }
+}
