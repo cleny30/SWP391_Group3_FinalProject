13f23b2e8957d76724c471f547e8f61b19d4e472
Le Duc An <anldce170717@fpt.edu.vn>
2023-10-10 10:58:23 +0700

    Add LoginController and AccountDAO for Login, add Account, Customer, AdminAndStaff, Addresses classes, change My account when logged in,

---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace SWP391_Group3_FinalProject.Models
+{
+    public class Account
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public int role {  get; set; }
+    }
+}
