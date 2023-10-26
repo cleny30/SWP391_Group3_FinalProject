98d5f2788ae4ab74c75e23e08d9f44720a5b56a0
Le Duc An <anldce170717@fpt.edu.vn>
2023-10-26 09:39:12 +0700

    Add form validate for add staff

--- wwwroot/js/AddStaff.js
+++ wwwroot/js/AddStaff.js
@@ -1,5 +1,6 @@
 ﻿var usernameExist = true;
 var emailExist = true;
+
 $('#Username').on('blur', function () {
     var username = $('#Username').val();
     if (username !== '') {
@@ -48,9 +49,135 @@ $('#Email').on('blur', function () {
     }
 });
 
+// Lấy giá tin input
+function getValueById(id) {
+    return $('#' + id).val();
+}
+
+// Hiển thị thông báo lỗi cho một trường
+function showError(id, message) {
+    $('#' + id).next('p').text(message);
+}
+
+// Ẩn thông báo lỗi của một trường
+function hideError(id) {
+    $('#' + id).next('p').text('');
+}
+// Kiểm tra tính hợp lệ của số điện thoại
+function isValidPhoneNumber(phoneNum) {
+    const phonePattern = /^0\d{9}$/;
+    return phonePattern.test(phoneNum);
+}
+// Kiểm tra tính hợp lệ của SSN
+function isValidSSN(SSN) {
+    const SSNPattern = /\d{12}$/;
+    return SSNPattern.test(SSN);
+}
+
+// Kiểm tra tính hợp lệ của địa chỉ email
+function isValidEmail(email) {
+    const emailPattern = /^[a-zA-Z][a-zA-Z0-9._%+-]+@[^\s@]+\.[^\s@]{2,}$/;
+    return emailPattern.test(email);
+}
+
+
+
 $(document).ready(function () {
     $('form.createAccount').submit(function (event) {
-        if (emailExist || usernameExist) {
+
+        var username = getValueById('Username');
+        var password = getValueById('Password');
+        var fullname = getValueById('Fullname');
+        var phone = getValueById('PhoneNumber');
+        var email = getValueById('Email');
+        var SSN = getValueById('SSN');
+        var address = getValueById('LivingAddress');
+
+        var noError = true;
+
+        //tài khoản
+        if (!username) {
+            showError('Username', 'Please enter your username!');
+            noError = false;
+        } else if (username.length > 20 || username.length < 6) {
+            showError('Username', 'Username must be from 6 to 20 characters!');
+            noError = false;
+        } else {
+            hideError('Username');
+        }
+
+
+        //Họ và tên
+        if (!fullname) {
+            showError('Fullname', 'Please enter your full name!');
+            noError = false;
+        } else if (fullname.length > 100) {
+            showError('Fullname', 'You full name must be less than 100 characters!');
+            noError = false;
+        } else {
+            hideError('Fullname');
+        }
+
+        // Số điện thoại
+        if (!phone) {
+            showError('PhoneNumber', 'Please enter your phone number!');
+            noError = false;
+        } else if (!isValidPhoneNumber(phone)) {
+            showError('PhoneNumber', 'Invalid phone number!');
+            noError = false;
+        } else {
+            hideError('PhoneNumber');
+        }
+
+        //Email
+        if (!email) {
+            showError('Email', 'Please enter your email address!');
+            noError = false;
+        } else if (!isValidEmail(email)) {
+            showError('Email', 'Invalid Email address!');
+            noError = false;
+        } else {
+            hideError('Email');
+        }
+
+        //Password
+        if (!password) {
+            showError('Password', 'Please enter a password!');
+            noError = false;
+        } else if (password.length < 8) {
+            showError('Password', 'Password must have at least 1 uppercase letter, 1 special character, and be at least 8 characters long!');
+            noError = false;
+        } else if (!/(?=.*[A-Z])(?=.*[!@#$%^&*])(.{8,})/.test(password)) {
+            showError('Password', 'Password must have at least 1 uppercase letter, 1 special character, and be at least 8 characters long!');
+            noError = false;
+        } else {
+            hideError('Password');
+        }
+
+        //SSN
+        if (!SSN) {
+            showError('SSN', 'Please enter your SSN!');
+            noError = false;
+        } else if (!isValidSSN(SSN)) {
+            showError('SSN', 'Invalid SSN!');
+            noError = false;
+        } else {
+            hideError('SSN');
+        }
+
+        //Địa chỉ
+        if (!address) {
+            showError('LivingAddress', 'Please enter your address!');
+            noError = false;
+        } else if (address.length > 200) {
+            showError('LivingAddress', 'address must be less than 200 characters!');
+            noError = false;
+        } else {
+            hideError('LivingAddress');
+        }
+
+        //If there is error, block form submit
+        if (!noError) {
             event.preventDefault();
         }
     });
